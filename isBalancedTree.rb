eba0e47d226881bb0795b8ace8d0f502b306b829
jstiven01 <stiven10@gmail.com>
2019-11-03 11:49:06 -0500

    Isbalanced Challenge

--- isBalancedTree.rb
+++ isBalancedTree.rb
@@ -9,7 +9,7 @@ end
 
 # helper function
 def array_to_tree(array, index = 0)
-	return nil if index >= array.length || array[index] == 0
+  return nil if index >= array.length || array[index] == 0
 
   node = Node.new(array[index])
   node.left = array_to_tree(array, 2*index+1)
@@ -19,17 +19,37 @@ def array_to_tree(array, index = 0)
 end
 
 # helper function
-def height(node)
-	# use your function from the previous challenge
+def height(node, max_height = 0, height = 1)
+    if node.nil?
+        if max_height < height - 1 then max_height = height - 1 end
+        return max_height
+    end
+    max_height = height(node.left, max_height, height + 1)
+    max_height = height(node.right, max_height, height + 1)
+    max_height
 end
 
 def balanced_tree?(array)
   root = array_to_tree(array)
-	# write your code here
+  queue = []
+  queue.push(root)
+
+  while !queue.empty?
+    node = queue.shift
+    p height(node.left) , height(node.right)
+    if (height(node.left) - height(node.right)).abs > 1
+        return false
+    end
+    if !node.left.nil? then queue.push(node.left) end
+
+    if !node.right.nil? then queue.push(node.right) end
+  end
+  return true
 end
 
-puts balanced_tree?([1, 2, 0, 3, 4, 0, 0])
+#puts balanced_tree?([1, 2, 3, 4, 5, 6, 7])
+puts balanced_tree?([1, 2, 3, 0, 0, 4, 5, 0, 0, 0, 0, 6, 0, 7, 8])
 # => false
 
-puts balanced_tree?([1, 2, 3, 4, 5, 6, 7])
+#puts balanced_tree?([1, 2, 3, 4, 5, 6, 7])
 # => true
